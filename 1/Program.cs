1442a66c6367deffaa617af76901eb974c49f81e
Dmitry <zimzulka1@gmail.com>
2022-09-16 00:17:31 +0400

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+for(int i=0; i<=b;i++)
+{
+    Console.WriteLine($"Число   {Math.Pow(a,i)}");
+}
+
+
